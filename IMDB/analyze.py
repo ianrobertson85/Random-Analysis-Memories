599d0f3ffb6936508b98956666caa70fd54580ea
Ian Robertson <ianrobertson85@gmail.com>
2015-05-19 16:24:37 -0400

    new changes

--- IMDB/analyze.py
+++ IMDB/analyze.py
@@ -7,6 +7,13 @@ def cleanup(filein):
     df = pd.DataFrame(eval(open(filein, 'r').read()))
     
     #Years look like (2015), convert to integer.
-    df['year'] = df['year'].apply(lambda x: int(x[1:5])
+    df['year'] = df['year'].apply(lambda x: int(x[1:5]))
     #Genres are split by a pipe
     df['genre'] = df['genre'].apply(lambda x: str(x).split('|'))
+     
+    return df
+
+
+if __name__ == '__main__':
+    df = cleanup('imdb_json.out')
+    print df[0:3] 

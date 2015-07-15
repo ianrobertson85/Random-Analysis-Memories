d786ed937b51e602f3806c2d71548b1b3c1157af
Ian Robertson <ianrobertson85@gmail.com>
2015-07-14 22:36:57 -0400

    analysis and new manipulations

--- lotto/manipulate.py
+++ lotto/manipulate.py
@@ -14,8 +14,32 @@ def handle_file(filename):
     dl = soup.dl
     for tr in soup.dl.find_all('tr')[1::]:
         match = tr.find_all('td')[0].text
+        if match == '3 + bonus':
+            match = '3_bonus'
+        elif match == '6 + bonus':
+            match = '6_bonus'
         val = int(tr.find_all('td')[1].text.replace(',', ''))
-        dicto[match] = val
+        dicto[match + '_winners'] = val
+
+        if match <> '3':
+            prize = int(float(tr.find_all('td')[2].text.replace(',', '').replace('$', '')))
+            dicto[match + '_prize'] = prize
+
+    try:
+        tab = soup.findAll('table', {'class': 'max-millions'})[0]
+        dicto['millions'] = len(tab.find_all('tr')) - 1
+    except:
+        dicto['millions'] = 0
+
+    dicto['jackpot'] = dicto['7_prize'] + dicto['millions'] * 1000000
+
+    try:
+        sales = soup.find('em', text='Total Sales').parent.nextSibling.nextSibling.nextSibling.text
+        sales = int(float(sales.replace(',', '').replace('$', '')) / 5)
+    except:
+        #There's a couple times where the total sales is missing.  Use odds of hitting 3 numbers there.
+        sales = dicto['3_winners'] * 8.1
+    dicto['sales'] = sales
 
     return dicto
 
@@ -24,8 +48,9 @@ if __name__ == '__main__':
 
     for filename in ['files/' + f for f in os.listdir('files')]:
         d = filename[6:16]
+        print d,
         overall_dict[d] = handle_file(filename)
-        print d
+        print 'done'
     with open('fileout.out', 'w') as f:
         f.write(str(overall_dict))
 
